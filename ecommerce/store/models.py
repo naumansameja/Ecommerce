d5735eaa015df39cbff8f4827578aaedd4c27c92
nauman.sameja <nauman.samejaa@gmail.com>
2024-05-30 23:01:33 +0500

    added image rendering for products in home page

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -16,12 +16,37 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False)
+    image = models.ImageField(null=True, blank=True)
+
+    @property
+    def imageURL(self):
+        if self.image:
+            return self.image.url
+        return ''
 
     def __str__(self):
         return self.name
     
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
-    date_ordered = models.DateField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=200, null=True)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zip_code = models.CharField(max_length=200, null=True)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+
