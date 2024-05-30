a3333ba5e7b4bad9f24c6b8edf473fe9b15e4518
nauman.sameja <nauman.samejaa@gmail.com>
2024-05-30 17:12:08 +0500

    added templates and models

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+]
